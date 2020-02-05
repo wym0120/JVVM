f0c16d2f7c48a0fa1446a27351720d304861e679
161250151 <161250151@smail.nju.edu.cn>
2020-02-06 00:45:57 +0800

    Create commandline util

---
+++ src/main/java/CommandLine.java
@@ -0,0 +1,16 @@
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+
+/**
+ * wym
+ */
+public class CommandLine {
+    private static CommandLineParser parser = new DefaultParser();
+
+    public static void readArgs(){
+        Options options = new Options();
+//        options.addOption();
+    }
+
+}
