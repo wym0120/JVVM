227980ef70543244e8abfe8a7da07facf103c7dc
161250170 <161250170@smail.nju.edu.cn>
2020-02-15 21:01:38 -0500

    constant pool -- fix

--- src/main/java/classloader/constantpool/ConstantPool.java
+++ src/main/java/classloader/constantpool/ConstantPool.java
@@ -3,8 +3,6 @@ package classloader.constantpool;
 import classloader.constantpool.info.ConstantPoolInfo;
 import com.sun.tools.javac.util.Pair;
 
-import java.util.ArrayList;
-
 /**
  * Description:
  *
@@ -14,12 +12,12 @@ import java.util.ArrayList;
 public class ConstantPool {
     private ConstantPoolInfo[] infos;
 
-    public static Pair<ConstantPool,Integer> getInstance(int constantPoolCnt, byte[] in, int offset) {
+    public static Pair<ConstantPool, Integer> getInstance(int constantPoolCnt, byte[] in, int offset) {
         ConstantPool constantPool = new ConstantPool();
         constantPool.infos = new ConstantPoolInfo[constantPoolCnt];
         int currentOfs = offset;
         int entryCnt = 0;
-        while (entryCnt < constantPoolCnt) {
+        while (entryCnt < constantPoolCnt - 1) {
             Pair<ConstantPoolInfo, Integer> infoInt = ConstantPoolInfo.getConstantPoolInfo(in, currentOfs);
             ConstantPoolInfo info = infoInt.fst;
             constantPool.infos[entryCnt] = info;
@@ -30,7 +28,11 @@ public class ConstantPool {
     }
 
     public ConstantPoolInfo getConstantPoolInfo(int i) {
-        ConstantPoolInfo info = infos[i];
+        if (i <= 0 || i > infos.length - 1) {
+            throw new UnsupportedOperationException("Invalid CP index " + i);
+        }
+
+        ConstantPoolInfo info = infos[i - 1];
         if (info == null) {
             throw new UnsupportedOperationException("Invalid CP index " + i);
         }
